65235b754193e04ce966c28887e6005872f0ef4c
Root Meet <rootmeet@gmail.com>
2023-10-14 15:13:26 -0600

    save

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import socket
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://mongo:27017/dev"
+app.config["MONGO_URI"] = "mongodb://10.102.43.39:27017/dev"
 mongo = PyMongo(app)
 db = mongo.db
 @app.route("/")
